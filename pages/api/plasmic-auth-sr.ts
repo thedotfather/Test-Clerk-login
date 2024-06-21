f314d742cac080a4cbe3233f4c9d2dcfacc07c62
StijnDeVosDEV <admin@thedotfather.com>
2024-06-21 20:36:53 +0200

    t

--- pages/api/plasmic-auth-sr.ts
+++ pages/api/plasmic-auth-sr.ts
@@ -5,7 +5,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-): Promise<void> {
+) {
 
-  res.json(await ExecuteEnsurePlasmicAppUser());
+  await ExecuteEnsurePlasmicAppUser();
+
+  res.status(200).json({ description: 'success' });
 }
