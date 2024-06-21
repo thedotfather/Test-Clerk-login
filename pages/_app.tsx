79dd1b53cb163422097b76d90a5f88b02ab868c5
StijnDeVosDEV <admin@thedotfather.com>
2024-06-21 09:59:42 +0200

    Clerk authentication added, no route protection yet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,20 @@ import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PlasmicRootProvider Head={Head}>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
+    <ClerkProvider>
+      <PlasmicRootProvider Head={Head}>
+        <Component {...pageProps} />
+      </PlasmicRootProvider>
+    </ClerkProvider>
   );
 }
