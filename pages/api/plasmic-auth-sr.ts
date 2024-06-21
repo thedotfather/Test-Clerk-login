7ccdd58a65de3d4f7d623c117f88fc5a4c61b776
StijnDeVosDEV <admin@thedotfather.com>
2024-06-21 20:26:07 +0200

    t

--- pages/api/plasmic-auth-sr.ts
+++ pages/api/plasmic-auth-sr.ts
@@ -2,7 +2,7 @@ import { ExecuteEnsurePlasmicAppUser } from '@/plasmic-auth';
 import type { NextApiRequest, NextApiResponse } from "next";
 
 // This API endpoint is used to provide the Plasmic user in client-side code.
-export default async function runPlasmicAuthDataHandler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
