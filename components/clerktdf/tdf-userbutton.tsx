ad9653692d4395cf90be2f98cddc93f94456d34f
StijnDeVosDEV <admin@thedotfather.com>
2024-06-21 12:15:11 +0200

    t

--- components/clerktdf/tdf-userbutton.tsx
+++ components/clerktdf/tdf-userbutton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 const TdfUserButton = () => {
   return (
