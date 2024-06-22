fe172cd3dc2297c83fb6ca0bf5519c2241af5773
StijnDeVosDEV <admin@thedotfather.com>
2024-06-22 15:58:33 +0200

    t

--- components/logoutButtonClerk.tsx
+++ components/logoutButtonClerk.tsx
@@ -4,11 +4,13 @@ import { SignOutButton } from '@clerk/nextjs';
 interface ClerkSignOutButtonProps {
   redirectUrl?: string;
   sessionId?: string;
+  children?: React.ReactNode;
 }
 
-export const ClerkSignOutButton: React.FC<ClerkSignOutButtonProps> = ({ redirectUrl, sessionId }) => {
+export const ClerkSignOutButton: React.FC<ClerkSignOutButtonProps> = ({ redirectUrl, sessionId, children }) => {
   return (
     <SignOutButton signOutOptions={{ sessionId }} redirectUrl={redirectUrl}>
+        {children}
     </SignOutButton>
   );
 };
